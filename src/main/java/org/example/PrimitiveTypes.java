474e2b3578b08f5b50f36387bfcb54ace4efbbbb
uxoa <palomita@gmail.com>
2024-10-29 17:33:40 +0100

    feat: boolean type

---
+++ src/main/java/org/example/PrimitiveTypes.java
@@ -0,0 +1,16 @@
+// Los tipos de datos primitivos
+
+package org.example;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        // Escribir un boolean, lo asignamos y pasar por consola
+        boolean isActive = false;
+
+        System.out.println(isActive);
+
+    }
+
+
+}
