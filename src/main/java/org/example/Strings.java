a600d860a40c053f9ba211d2b1983772c56f6e18
uxoa <palomita@gmail.com>
2024-10-29 18:15:19 +0100

    Feat: Strings

---
+++ src/main/java/org/example/Strings.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        // Imprimir por consola una comparacion de dos strings con el metodo equals
+        String name = "Paloma";
+        String surname = new String("Babot");
+        String fullName = name + " " + surname;
+
+        System.out.println(fullName);
+    }
+}
