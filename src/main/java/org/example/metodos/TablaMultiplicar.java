1048e50c79048056b68610bd83f9f8570512b9f0
uxoa <palomita@gmail.com>
2024-11-05 20:40:48 +0100

    Feat: algunos ejercicios con Métodos

---
+++ src/main/java/org/example/metodos/TablaMultiplicar.java
@@ -0,0 +1,22 @@
+package org.example.metodos;
+
+import java.util.Scanner;
+
+public class TablaMultiplicar {
+    Scanner scanner = new Scanner(System.in);
+    int numero = scanner.nextInt();
+
+    public void mostrarTablaMultiplicar(){
+       for (int i = 0; i < 11; i++){
+           System.out.println(numero * i);
+       }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Escribe un numero para ver su Tabla de Multiplicar");
+        TablaMultiplicar tabla = new TablaMultiplicar();
+        tabla.mostrarTablaMultiplicar();
+    }
+
+}
