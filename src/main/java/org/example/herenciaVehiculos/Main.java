ad09194e6449f2bea300ad2b61df3b0ed8d1b9c1
uxoa <palomita@gmail.com>
2024-11-05 15:09:42 +0100

    Fix: Ejercicio Herencia vehículo

--- src/main/java/org/example/herenciaVehiculos/Main.java
+++ src/main/java/org/example/herenciaVehiculos/Main.java
@@ -3,18 +3,34 @@ package org.example.herenciaVehiculos;
 public class Main {
     public static void main(String[] args) {
         Car car1 = new Car("Seat", 10, (byte) 4);
+        Moto moto1 = new Moto("Impala", 30, (byte) 2);
 
+        // Car Llamar al método acelerar
+        car1.acelerar();
         System.out.println("Marca: " + car1.getMarca());
         System.out.println("Velocidad inicial: " + car1.getVelocity());
         System.out.println("Ruedas: " + car1.getWheels());
-
-        // Llamar al método acelerar
-        car1.acelerar();
-        System.out.println("Velocidad después de acelerar: " + car1.getVelocity());
-
-        // Llamar al método frenar
+        System.out.println("Velocidad después de acelerar: " + moto1.getVelocity());
+        // Car Llamar al método frenar
         car1.frenar();
         System.out.println("Velocidad después de frenar: " + car1.getVelocity());
+
+        System.out.println("---------------------------------");
+
+        // moto llama a acelerar
+        moto1.acelerar();
+
+        System.out.println("Marca: " + moto1.getMarca());
+        System.out.println("Velocidad inicial: " + moto1.getVelocity());
+        System.out.println("Ruedas: " + moto1.getWheels());
+
+        System.out.println("Velocidad después de acelerar: " + moto1.getVelocity());
+       // moto llama a frenar
+        moto1.frenar();
+        System.out.println("Velocidad después de frenar: " + moto1.getVelocity());
+
+
+
     }
 }
 
--- src/main/java/org/example/herenciaVehiculos/Moto.java
+++ src/main/java/org/example/herenciaVehiculos/Moto.java
@@ -1,10 +1,20 @@
 package org.example.herenciaVehiculos;
 
 public class Moto extends Vehiculo{
-
-
     public Moto(String marca, int velocity, byte wheels) {
         super(marca, velocity, wheels);
+    }
+
+
+    @Override
+    public void acelerar() {
+        setVelocity(getVelocity() + 5); // Aumenta en 20 para el coche
+    }
+
+    @Override
+    public void frenar() {
+        // Reduce la velocidad en 15 en lugar de 10
+        setVelocity(Math.max(getVelocity() - 30, 0));
     }
 }
 
